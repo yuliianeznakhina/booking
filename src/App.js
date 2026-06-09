d6d4c9c44cdabc5ccd264f3f6d8d1a341a223898
Your Name <you@example.com>
2026-06-09 10:20:46 +0300

    started header css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import { PropertyCard } from './components/PropertyCard/PropertyCard';
+import { Header } from './components/Header/Header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
     </div>
   );
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,20 @@
 import './Header.css';
 
-const Header = () => (
-  <div className="Header">
-    Header Component
-  </div>
-);
+const countries = [
+  {image:'./images/icons/flag-UA.png', name:'UA'},
+  {image:'./images/icons/flag-USA.png', name:'USA'},
+  {image:'./images/icons/flag-TR.png', name:'TR'},
+  {image:'./images/icons/flag-CH.png', name:'CH'},
+  {image:'./images/icons/flag-IND.png', name:'IND'},
+  {image:'./images/icons/flag-DN.png', name:'DN'}
+]
 
-export default Header;
+export function Header() {
+  return (<header className="Header">
+    <select id='country'>
+      {countries.map(elem=><option value={elem.name}><img src={elem.image}/></option>)}
+    </select>
+    <button className='appBTN'>Open app<img src='./images/icons/open-app.png'/></button>
+  </header>
+  );
+}
