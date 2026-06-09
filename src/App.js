3e4cde3b3c653976a264a6177dfe8be00f062310
Your Name <you@example.com>
2026-06-09 09:38:59 +0300

    added header component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <div className='CardWrapper'>
-      <PropertyCard image='https://media.vrbo.com/lodging/19000000/18780000/18770500/18770484/eccebe6c.jpg?impolicy=fcrop&w=600&h=338&q=medium&p=1' title='Villa in Corfu' bedrooms='3' rating='10' reviews='62' price='770' fees='All fees included' dateStart='Oct 1' dateEnd='Oct 5' />
-      <PropertyCard image='https://media.vrbo.com/lodging/19000000/18780000/18770500/18770484/eccebe6c.jpg?impolicy=fcrop&w=600&h=338&q=medium&p=1' title='Villa in Corfu' bedrooms='3' rating='10' reviews='62' price='770' fees='All fees included' dateStart='Oct 1' dateEnd='Oct 5' />
-      <PropertyCard image='https://media.vrbo.com/lodging/19000000/18780000/18770500/18770484/eccebe6c.jpg?impolicy=fcrop&w=600&h=338&q=medium&p=1' title='Villa in Corfu' bedrooms='3' rating='10' reviews='62' price='770' fees='All fees included' dateStart='Oct 1' dateEnd='Oct 5' />
-      <PropertyCard image='https://media.vrbo.com/lodging/19000000/18780000/18770500/18770484/eccebe6c.jpg?impolicy=fcrop&w=600&h=338&q=medium&p=1' title='Villa in Corfu' bedrooms='3' rating='10' reviews='62' price='770' fees='All fees included' dateStart='Oct 1' dateEnd='Oct 5' />
-      </div>
+      
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,9 @@
+import './Header.css';
+
+const Header = () => (
+  <div className="Header">
+    Header Component
+  </div>
+);
+
+export default Header;
