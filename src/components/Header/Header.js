a0f3ac788a6721d8ae930ebc23101e0cff405a74
Your Name <you@example.com>
2026-06-09 11:55:03 +0300

    header css

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,27 +1,42 @@
 import './Header.css';
+import { useState } from 'react';
 
-const countries = [
-  {image:'./images/icons/flag-UA.png', name:'UA'},
-  {image:'./images/icons/flag-USA.png', name:'USA'},
-  {image:'./images/icons/flag-TR.png', name:'TR'},
-  {image:'./images/icons/flag-CH.png', name:'CH'},
-  {image:'./images/icons/flag-IND.png', name:'IND'},
-  {image:'./images/icons/flag-DN.png', name:'DN'}
-]
 
 export function Header() {
+  const countries = [
+    { image: './images/icons/flag-UA.png', name: 'UA' },
+    { image: './images/icons/flag-USA.png', name: 'USA' },
+    { image: './images/icons/flag-TR.png', name: 'TR' },
+    { image: './images/icons/flag-CH.png', name: 'CH' },
+    { image: './images/icons/flag-IND.png', name: 'IND' },
+    { image: './images/icons/flag-DN.png', name: 'DN' }
+  ]
+
+  const [selected, setSelected] = useState(countries[0]);
+  const [open, setOpen] = useState(false);
+
   return (<header className="Header">
-    <select id='country'>
-      {countries.map(elem=><option value={elem.name}><img src={elem.image}/></option>)}
-    </select>
-    <button className='appBTN'>Open app<img src='./images/icons/open-app.png'/></button>
-  <nav>
-    <a>Trip Boards</a>
-    <a>List your property</a>
-    <a>Help</a>
-    <a>My trips</a>
-  </nav>
-  <button className='signInBTN'>Sign in</button>
+    <div id='countrySelect'>
+      <div className='countrySelected' onClick={() => setOpen(!open)}> <img src={selected.image} />{selected.name}<img src='./images/icons/arrow-down.png'/></div>
+      {open && (
+        <div className='countryOption'>
+          {countries.map((country) => (<div
+            onClick={() => {
+              setSelected(country);
+              setOpen(false);
+            }}>
+            <img src={country.image} />
+          </div>))}
+        </div>)}
+    </div>
+    <button className='appBTN'>Open app<img src='./images/icons/open-app.png' /></button>
+    <nav>
+      <a>Trip Boards</a>
+      <a>List your property</a>
+      <a>Help</a>
+      <a>My trips</a>
+    </nav>
+    <button className='signInBTN'>Sign in</button>
   </header>
   );
 }
