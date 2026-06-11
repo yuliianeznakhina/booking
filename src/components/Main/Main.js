f18ed52c5e333ac6113e96d29acad645632c25fb
Your Name <you@example.com>
2026-06-11 11:53:52 +0300

    main page

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,15 +1,21 @@
 import './Main.css';
 import { Header } from './Header/Header';
+import { Find } from './Find/Find';
 import { About } from './About/About';
 import { SignInBanner } from './SignInBanner/SignInBanner';
+import FQL from './FQL/FQL';
+import Footer from './Footer/Footer';
 
 export function Main() {
   return (<>
     <Header />
+    <Find />
     <div className="Main">
       <About />
       <SignInBanner />
+      <FQL />
     </div>
+    <Footer />
   </>
   );
 }
