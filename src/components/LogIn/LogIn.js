d41aa08b7338bc86e116250588bc62bab943bdea
Your Name <you@example.com>
2026-06-12 10:18:55 +0300

    login css

--- src/components/LogIn/LogIn.js
+++ src/components/LogIn/LogIn.js
@@ -24,46 +24,53 @@ export function LogIn(props) {
     }
   }
 
-  return (<div className='wrap'>
+  return (<div className='loginWrap'>
     <Link className='backBTN' to="/"><img src='./images/icons/arrow-left.svg' /></Link>
-    <div className="LogIn">
-      <h2>Sign in or log in</h2>
-      <h4>Unlock a world of rewards with one account across Expedia</h4>
-      <form>
-        <label className='textInput' htmlFor='email'>
-          <img src='./images/icons/email.svg' />
-          <input id='email' type='email' placeholder='Email' />
-        </label>
-        <label className='textInput' htmlFor='password'>
-          <img src='./images/icons/password.svg' />
-          <input id='password' type={password.show ? 'password' : 'text'} placeholder='Password' />
-          <img onClick={togglePassword} src={password.img} />
-        </label>
-        {props.mode == 'Signin' &&
-          <label className='textInput' htmlFor='confirmPassword'>
+    <div className='loginCenter'>
+      <div className="LogIn">
+        <div className='loginTitle'>
+          <h2>Sign in or log in</h2>
+          <h4>Unlock a world of rewards with one account across Expedia</h4>
+        </div>
+        <form>
+          <label className='textInput' htmlFor='email'>
+            <img src='./images/icons/email.svg' />
+            <input id='email' type='email' placeholder='Email' />
+          </label>
+          <label className='textInput' htmlFor='password'>
             <img src='./images/icons/password.svg' />
-            <input id='confirmPassword' type={confirmPassword.show ? 'password' : 'text'} placeholder='Password' />
-            <img onClick={toggleConfirmPassword} src={confirmPassword.img} />
-          </label>}
+            <input id='password' type={password.show ? 'password' : 'text'} placeholder='Password' />
+            <img onClick={togglePassword} src={password.img} />
+          </label>
+          {props.mode == 'Signin' &&
+            <label className='textInput' htmlFor='confirmPassword'>
+              <img src='./images/icons/password.svg' />
+              <input id='confirmPassword' type={confirmPassword.show ? 'password' : 'text'} placeholder='Password' />
+              <img onClick={toggleConfirmPassword} src={confirmPassword.img} />
+            </label>}
+          {props.mode == 'Signin' &&
+            <label htmlFor='checkbox' className='checkboxContainer'>
+              <input id='checkbox' type='checkbox' required />
+              <span class="checkmark"></span>
+              <h4 className='link'>I predict the situation with <Link>local residents</Link> and <Link>privacy policy</Link></h4>
+            </label>}
+
+          {props.mode == 'Login' &&
+            <span className='loginCheckbox link'>
+              <label htmlFor='checkbox'><input id='checkbox' type='checkbox' /><h4>Remember me</h4></label>
+              <a>Forgot Password or Email?</a>
+            </span>}
+
+          <Link className='signInBTN' to="/">{props.mode == 'Signin' ? "Sign up" : "Log in"}</Link>
+        </form>
+        <h4>Other ways to log in</h4>
+        <button className='altLogin'><img src='./images/icons/google-logo.svg' /><span>Continue with google</span></button>
+        <button className='altLogin'><img src='./images/icons/apple-logo.svg' /><span>Continue with apple</span></button>
+        {props.mode == 'Login' &&
+          <h4 className='link'>Don’t have an account? <Link to='/Signin'>Sign up</Link></h4>}
         {props.mode == 'Signin' &&
-          <label htmlFor='checkbox' className='checkboxContainer'>
-            <input id='checkbox' type='checkbox' required />
-            <span class="checkmark"></span>
-            <h4>I predict the situation with <a>local residents</a> and <a>privacy policy</a></h4>
-          </label>}
-        {props.mode == 'Login' &&
-          <label htmlFor='checkbox'><input id='checkbox' type='checkbox' /><h4>Remember me</h4></label>}
-        {props.mode == 'Login' &&
-          <a>Forgot Password or Email?</a>}
-        <Link className='signInBTN' to="/">{props.mode == 'Signin' ? "Sign up" : "Log in"}</Link>
-      </form>
-      <h4>Other ways to log in</h4>
-      <button><img />Continue with google</button>
-      <button><img />Continue with apple</button>
-      {props.mode == 'Login' &&
-        <h4>Already have an account? <a>Log in</a></h4>}
-      {props.mode == 'Signin' &&
-        <h4>Don’t have an account? <a>Sign up</a></h4>}
+          <h4 className='link'>Already have an account? <Link to='/Login'>Log in</Link></h4>}
+      </div>
     </div>
   </div>
   );
