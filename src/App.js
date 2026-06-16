dd554d48da3db4022187625c5136c3e5a12aec89
Your Name <you@example.com>
2026-06-16 12:42:45 +0300

    added information page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { createContext, useState, useEffect } from 'react';
 import { Main } from './components/Main/Main';
 import { LogIn } from './components/LogIn/LogIn';
 import RentiqPage from "./components/Rentiqpage/Rentiqpage";
+import InformationPage from "./components/InformationPage/InformationPage";
 import './App.css';
 
 export const LayoutContext = createContext();
@@ -16,6 +17,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Main />} />
         <Route path="/MoreAboutOneKey" element={<RentiqPage />} />
+        <Route path="/InformationPage" element={<InformationPage />} />
         <Route path="/Signin" element={<LogIn mode='Signin' />} />
         <Route path="/Login" element={<LogIn mode='Login' />} />
       </Routes>
